3429d41034adb0bd0cee275948b5cb9b429308de
WonilChoi <nbcross@naver.com>
2024-01-24 23:50:49 +0900

    first commit

---
+++ gallery_list.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>갤러리</title>
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <?php include 'menu.html'; ?>
+    <div class="wrapper">
+<?php
+    $d = dir("./upload");
+    while($file = $d->read()) {
+        if($file == '.' or $file == '..') {
+            continue;
+        }
+        $arr = explode('.', $file);
+        $ext = end($arr);
+        if($ext == 'jpg' or $ext == 'JPG' or $ext == 'png' or $ext == 'PNG') {
+            echo '
+            <div class="img_div">
+                <img src="upload/'.$file.'" width="100">
+            </div>
+            ';
+        }
+    }
+
+?>
+
+    </div>
+</body>
+</html>
